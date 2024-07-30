1cc4337143b0888b5e67fea1fab4f320278a1e88
leojay-net <aleonomohjoseph03@gmail.com>
2024-07-30 22:04:11 +0100

    model update

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+import uuid
 class ChatHistory(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     session_key = models.CharField(max_length=40, blank=True)
     email = models.EmailField(blank=False, null=False)
     input = models.TextField()
